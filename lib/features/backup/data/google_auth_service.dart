44775268cab13611eba6383aa09f70e44abeb9c8
amaralfatah <amaralfatah.main@gmail.com>
2026-07-18 08:37:10 +0700

    feat: add GoogleAuthService wrapping google_sign_in for Drive scope

---
+++ lib/features/backup/data/google_auth_service.dart
@@ -0,0 +1,65 @@
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:http/http.dart' as http;
+
+class GoogleAuthService {
+  static const List<String> scopes = <String>[
+    'https://www.googleapis.com/auth/drive.appdata',
+  ];
+
+  final GoogleSignIn _signIn = GoogleSignIn.instance;
+  bool _initialized = false;
+
+  Stream<GoogleSignInAuthenticationEvent> get authenticationEvents =>
+      _signIn.authenticationEvents;
+
+  Future<void> ensureInitialized() async {
+    if (_initialized) {
+      return;
+    }
+    await _signIn.initialize();
+    _initialized = true;
+  }
+
+  Future<GoogleSignInAccount?> attemptSilentSignIn() async {
+    await ensureInitialized();
+    return _signIn.attemptLightweightAuthentication();
+  }
+
+  Future<GoogleSignInAccount> signIn() async {
+    await ensureInitialized();
+    return _signIn.authenticate(scopeHint: scopes);
+  }
+
+  Future<void> disconnect() async {
+    await ensureInitialized();
+    await _signIn.disconnect();
+  }
+
+  Future<http.Client?> authenticatedHttpClient(
+    GoogleSignInAccount account, {
+    bool promptIfNecessary = false,
+  }) async {
+    final authorization = promptIfNecessary
+        ? await account.authorizationClient.authorizeScopes(scopes)
+        : await account.authorizationClient.authorizationForScopes(scopes);
+
+    if (authorization == null) {
+      return null;
+    }
+
+    return _BearerTokenClient(authorization.accessToken);
+  }
+}
+
+class _BearerTokenClient extends http.BaseClient {
+  _BearerTokenClient(this._accessToken);
+
+  final String _accessToken;
+  final http.Client _inner = http.Client();
+
+  @override
+  Future<http.StreamedResponse> send(http.BaseRequest request) {
+    request.headers['Authorization'] = 'Bearer $_accessToken';
+    return _inner.send(request);
+  }
+}
