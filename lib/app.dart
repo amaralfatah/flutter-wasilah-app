7c7cbc84fb11fcaf00152c41bca997cf33346c28
amaralfatah <amaralfatah.main@gmail.com>
2026-07-18 09:11:05 +0700

    feat: trigger auto-backup on app launch and resume

--- lib/app.dart
+++ lib/app.dart
@@ -2,13 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_wasilah_app/core/router/app_router.dart';
 import 'package:flutter_wasilah_app/core/theme/app_theme.dart';
+import 'package:flutter_wasilah_app/features/backup/providers/backup_controller.dart';
 import 'package:flutter_wasilah_app/features/settings/providers/theme_mode_provider.dart';
 
-class App extends ConsumerWidget {
+class App extends ConsumerStatefulWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<App> createState() => _AppState();
+}
+
+class _AppState extends ConsumerState<App> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      ref.read(backupControllerProvider.notifier).maybeAutoBackup();
+    });
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      ref.read(backupControllerProvider.notifier).maybeAutoBackup();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final router = ref.watch(appRouterProvider);
     final themeMode = ref.watch(themeModeProvider);
 
